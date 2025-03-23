506f463071e59ac3c046717f559e6e65c5fe1437
Rrosso27 <125314912+Rrosso27@users.noreply.github.com>
2025-03-23 15:52:46 -0500

    login

---
+++ resources/views/components/input-error.blade.php
@@ -0,0 +1,9 @@
+@props(['messages'])
+
+@if ($messages)
+    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}>
+        @foreach ((array) $messages as $message)
+            <li class="p-3 font-bold text-red-600 bg-red-100 border-l-4 border-red-600">{{ $message }}</li>
+        @endforeach
+    </ul>
+@endif
---
+++ resources/views/components/link.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+        {{ __('Forgot your password?') }}
+    </a>
+</div>
